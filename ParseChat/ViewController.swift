2683e0221f11d59fd58223fbde8c1a2bcbd3f50a
Yao Yu <yao97ming@gmail.com>
2020-03-13 16:13:43 -0700

    Initial Commit

---
+++ ParseChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Yao Yu on 3/13/20.
+//  Copyright © 2020 Yao Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
